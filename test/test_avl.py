a4d08b8ef33a1863f5cd06d9920c9981b23b68ab
Marco Pensallorto <marco.pensallorto@gmail.com>
2012-03-18 13:29:13 +0100

    Drafting python test suite

---
+++ test/test_avl.py
@@ -0,0 +1,86 @@
+import unittest
+import avl
+
+class testAvl(unittest.TestCase):
+    """A test class for the avl module.
+    """
+
+    def setUp(self):
+
+        """set up data used in the tests. setUp is called before each
+        test function execution.
+        """
+        self.avl_tree = avl.Avl()
+
+
+    def testKeyInsertion(self):
+        title = "insertion"
+        self.avl_tree.insert(42, "Forty-two")
+
+        self.assertTrue(42 in self.avl_tree)
+
+    # def testGetFeedPostingURL(self):
+
+    #     posting_url = "http://www.avl_tree.com/atom/9276918"
+
+    #     self.assertEqual(self.avl_tree.get_feed_posting_url(), posting_url)
+
+
+
+    # def testGetFeedPostingHost(self):
+
+    #     posting_host = "www.avl_tree.com"
+
+    #     self.assertEqual(self.avl_tree.get_feed_posting_host(), posting_host)
+
+
+
+    # def testPostNewEntry(self):
+
+    #     init_num_entries = self.avl_tree.get_num_entries()
+
+    #     title = "testPostNewEntry"
+
+    #     content = "testPostNewEntry"
+
+    #     self.assertTrue(self.avl_tree.post_new_entry(title, content))
+
+    #     self.assertEqual(self.avl_tree.get_num_entries(), init_num_entries+1)
+
+    #     # Entries are ordered most-recent first
+
+    #     # Newest entry should be first
+
+    #     self.assertEqual(title, self.avl_tree.get_nth_entry_title(1))
+
+    #     self.assertEqual(content, self.avl_tree.get_nth_entry_content_strip_html(1))
+
+
+
+    # def testDeleteAllEntries(self):
+
+    #     self.avl_tree.delete_all_entries()
+
+    #     self.assertEqual(self.avl_tree.get_num_entries(), 0)
+
+
+def suite():
+
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(testavl))
+
+    return suite
+
+if __name__ == '__main__':
+
+    unittest.main()
+
+    # suiteFew = unittest.TestSuite()
+
+    # suiteFew.addTest(testavl("testPostNewEntry"))
+
+    # suiteFew.addTest(testavl("testDeleteAllEntries"))
+
+    # #unittest.TextTestRunner(verbosity=2).run(suiteFew)
+
+    # unittest.TextTestRunner(verbosity=2).run(suite())
